0e0fb2509e85c73a368dde404a69204b0c84a00d
Colin Leung <colin@cowise.co>
2024-04-25 04:39:34 +0800

    fix: index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import url from "node:url";
 const dirname = url.fileURLToPath(new URL('.', import.meta.url)).replace(/\/$/, '');
 export default {dirname}
 
-import ControllerMixinSession from './classes/controller-mixin/Session';
-import Session from './classes/Session';
-import ModelSession from './classes/model/Session';
+import ControllerMixinSession from './classes/controller-mixin/Session.mjs';
+import Session from './classes/Session.mjs';
+import ModelSession from './classes/model/Session.mjs';
 
 export{
   ControllerMixinSession,
