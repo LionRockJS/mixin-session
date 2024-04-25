a2adb2c803d3b8880f6cdb4b1fb6695968ce79a6
Colin Leung <colin@cowise.co>
2024-04-26 05:10:29 +0800

    fix: init.js import file without .mjs

--- init.mjs
+++ init.mjs
@@ -1,6 +1,6 @@
 import Central from '@lionrockjs/central';
 
 Central.initConfig(new Map([
-  ['cookie', await import('./config/cookie')],
-  ['session', await import('./config/session')],
+  ['cookie', await import('./config/cookie.mjs')],
+  ['session', await import('./config/session.mjs')],
 ]));
