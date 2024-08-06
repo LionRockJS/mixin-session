e47bf034665fcb8d8956c78caec32c4a9c56b7fd
Colin Leung <colin@cowise.co>
2024-08-07 03:40:29 +0800

    chore: add test database

--- tests/test1/application/classes/controller/Session.mjs
+++ tests/test1/application/classes/controller/Session.mjs
@@ -1,9 +1,8 @@
 import { Controller } from '@lionrockjs/mvc';
-import { ControllerMixinDatabase } from '@lionrockjs/central';
 import ControllerMixinSession from '../../../../../classes/controller-mixin/Session';
 
 export default class ControllerSession extends Controller{
-  static mixins = [ControllerMixinDatabase, ControllerMixinSession];
+  static mixins = [ControllerMixinSession];
 
   constructor(request){
     super(request);
