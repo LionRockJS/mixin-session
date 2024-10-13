a64328c69360af8f7aa8b3b5eafdff7a9fc2ead6
Colin Leung <colin@cowise.co>
2024-10-13 11:37:52 +0800

    feat: v2 filename and configs

--- init.mjs
+++ init.mjs
@@ -1,6 +1,1 @@
-import Central from '@lionrockjs/central';
-
-await Central.initConfig(new Map([
-  ['cookie', await import('./config/cookie.mjs')],
-  ['session', await import('./config/session.mjs')],
-]));
+export default {}
